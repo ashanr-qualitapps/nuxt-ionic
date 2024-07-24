a1b5ca2cf2d1acdbc82666a53ae542e44edc61db
ashanr-qualitapps <ashan.rajapaksha@qualitapps.com>
2024-07-24 02:40:01 +0000

    added capacitor configs

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.qualitapps.nuxtmobile',
+  appName: 'nuxt-ionic-app',
+  webDir: 'dist'
+};
+
+export default config;
